2d102897159fe2b1491df3f4fa9674d479031feb
Andy Dong <dong.andy25@outlook.com>
2022-02-07 15:45:22 -0500

    Added ISpriteFactory, SpriteConstants, and images

---
+++ sprint2/ISpriteFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSE3902_Sprint2
+{
+    public interface ISpriteFactory
+    {
+        void LoadAllResources(ContentManager content);
+    }
+}
