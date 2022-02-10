59629f7d9f9a67a3477986a0a06fdc3220dfff1d
Devan Mallory <32146940+dman82499@users.noreply.github.com>
2022-02-09 20:46:04 -0500

    added shells for IItems

---
+++ sprint2/Items/BasicItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSE3902_Sprint2.Objects.Player
+
+namespace sprint2
+{
+    public abstract class BasicItem
+    {
+        public int increaseAmount = 3;
+
+        public Player boostedPlayer;
+
+    }
+}
---
+++ sprint2/Items/IItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+
+namespace sprint2
+{
+    interface IItem
+    {
+        void Activate();
+
+        void Update();
+        
+        public Rectangle getSprite();
+
+    }
+}
