538a53ee6e9a06c579e7c1dbe937934ab5e5ee6e
AlxJiachen <zhang.11945@buckeyemail.osu.edu>
2022-02-16 03:42:36 -0500

    Background pic and gamestate
    I added the background picture.
    
    Tried to have a timer on screen, but failed. Leave some pieces of code in Update() section
    
    Tried let character do not move out of screen, failed. Build a "safebound" instead

--- sprint2/Game1.cs
+++ sprint2/Game1.cs
@@ -14,19 +14,38 @@ using CSE3902_Sprint2.Objects.Items;
 using sprint2.Sprites.Decorations;
 using System;
 using sprint2.Objects.NPC;
+using Microsoft.Xna.Framework.Content;
 
 namespace CSE3902_Sprint2
 {
 
     public class Game1 : Game
     {
+        public int Timeplayed;
+        public int score;
+        Rectangle safeBounds;
+        const float safeAreaPortion = 0.001f;
+        int TimeofGame = 0;
+        Boolean isPaused = false;
+
         public GraphicsDeviceManager graphics;
+        public static ContentManager contentManager;
         public SpriteBatch spriteBatch;
         private ArrayList controllerList;
         public Player player1;
         public Player player2;
         //public Player npcPlayer;
         public Enemy enemy;
+        GameState currentGameState;
+        
+
+       enum GameState
+        {
+            GameMenu = 0,
+            GamePlay = 1,
+            GameOver = 2,
+            GamePause = 3,
+        }
 
 
         private ISprite destructableBlockSprite { get; set; }
@@ -49,24 +68,36 @@ namespace CSE3902_Sprint2
 
         public List<BasicItem> currentItemList;
         public int currItemIndex = 0;
+        private SpriteFont font;
 
         public Game1()
-        {
+        {         
             graphics = new GraphicsDeviceManager(this);
             graphics.PreferredBackBufferHeight = 768;
             graphics.PreferredBackBufferWidth = 1024;
             graphics.ApplyChanges();
             screenSize = new Vector2(graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
             Content.RootDirectory = "Content";
+            
         }
 
         protected override void Initialize()
         {
+            Viewport viewport = graphics.GraphicsDevice.Viewport;
+            safeBounds = new Rectangle(
+                (int)(viewport.Width * safeAreaPortion),
+                (int)(viewport.Height * safeAreaPortion),
+                (int)(viewport.Width * (1 - 2 * safeAreaPortion)),
+                (int)(viewport.Height * (1 - 2 * safeAreaPortion)));
+            //TODO change in the future
+            currentGameState = GameState.GamePlay;
+            
+
+
             player1 = new Player(new Vector2(30, 30), this);
             player2 = new Player(new Vector2(700, 30), this);
             //npcPlayer = new NPCPlayer(new Vector2(450, 300), this, randomNum); 
             enemy = new Enemy(new Vector2(450, 300), this);
-
 
             controllerList = new ArrayList();
             controllerList.Add(new KeyboardController(this, player1, player2, enemy));
@@ -89,6 +120,7 @@ namespace CSE3902_Sprint2
         {
 
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            SpriteFont font = Content.Load<SpriteFont>("Score");
 
             destructableBlockTexture = Content.Load<Texture2D>("DestructableCrate");
             indestructableBlockTexture = Content.Load<Texture2D>("Concrete_Block_2x2");
@@ -116,27 +148,102 @@ namespace CSE3902_Sprint2
 
         protected override void Update(GameTime gameTime)
         {
-            foreach (IController controller in controllerList)
-            {
-                controller.Update();
-            }
-            player1.Update();
-            player2.Update();
-            enemy.Update();
+            KeyboardState keyboard = Keyboard.GetState();
+            GamePadState gamePad = GamePad.GetState(PlayerIndex.One);
 
-            base.Update(gameTime);
+            
+            switch (currentGameState)
+            {
+                case GameState.GameMenu:
+
+                    if (gamePad.Buttons.Back == ButtonState.Pressed ||
+                        keyboard.IsKeyDown(Keys.Escape))
+                    {
+                        this.Exit();
+                    }
+                    if (gamePad.Buttons.Start == ButtonState.Pressed ||
+                        keyboard.IsKeyDown(Keys.Enter))
+                    {
+
+
+                        // Set the game state to play
+                        currentGameState = GameState.GamePlay;
+
+                        // Reset score
+                        score = 0;
+                    }
+                    break;
+
+                case GameState.GameOver:
+                    // If game is over, the game allows return to main menu if key A is pressed 
+                    if (gamePad.Buttons.A == ButtonState.Pressed ||
+                        keyboard.IsKeyDown(Keys.Enter))
+                    {
+                        currentGameState = GameState.GameMenu;
+                    }
+                    break;
+
+                case GameState.GamePlay:
+                    if (keyboard.IsKeyDown(Keys.P))
+                    {
+                        isPaused = !isPaused;
+                    }
+                    if (!isPaused)
+                    {   
+                        TimeofGame += gameTime.ElapsedGameTime.Milliseconds;
+                       
+                        //code when the game isn't paused.
+                    }
+                    else
+                    {
+                        //code when the game is paused.
+                        currentGameState = GameState.GamePause;
+                    }
+                    // Press X or I during game play to return to main menu
+                    if (gamePad.Buttons.X == ButtonState.Pressed ||
+                        keyboard.IsKeyDown(Keys.I))
+                    {
+                        currentGameState = GameState.GameMenu;
+                    }
+                    // Press ESC to quit game during game play
+                    if (gamePad.Buttons.Back == ButtonState.Pressed ||
+                keyboard.IsKeyDown(Keys.Escape))
+                    {
+                        this.Exit();
+                    }
+                    foreach (IController controller in controllerList)
+                    {
+                        controller.Update();
+                    }
+                    player1.Update();
+                    player2.Update();
+                    enemy.Update();
+
+                    
+                    break;
+
+
+                    base.Update(gameTime);
+
+
+
+            }
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+                        
             this.IsMouseVisible = true;
             destructableBlockSprite.Draw(spriteBatch, new Vector2(250, 250));
             indestructableBlockSprite.Draw(spriteBatch, new Vector2(280, 250));
             tree1.Draw(spriteBatch, new Vector2(310, 250));
             tree2.Draw(spriteBatch, new Vector2(350, 250));
-
             spriteBatch.Begin();
+            
+
+            Texture2D background = Content.Load<Texture2D>("background2"); ;
+            //spriteBatch.Draw(background, new Rectangle(0, 0, 800, 480), Color.White);
+            spriteBatch.Draw(background, safeBounds, Color.White);
 
             player1.Draw(spriteBatch);
             player2.Draw(spriteBatch);
