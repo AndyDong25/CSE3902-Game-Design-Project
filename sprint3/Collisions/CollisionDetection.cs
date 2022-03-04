fc2d1afd74e9251154ccf5be1b64ee9374b19f12
cskyl <chen.9471@osu.edu>
2022-03-03 19:38:33 -0800

    add ninjastar

--- sprint3/Collisions/CollisionDetection.cs
+++ sprint3/Collisions/CollisionDetection.cs
@@ -260,7 +260,6 @@ namespace sprint2.Collisions
                         n.collisionHandler.HandleCollision(n);
                         //(p1.collisionHandler as PlayerBlockCollisionHandler).HandleCollision(p1);
                     }
-                   
                 }
             }
             
