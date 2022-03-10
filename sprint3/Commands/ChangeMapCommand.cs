fe1e8a3e4fa7f891efa5c465ef8139333d10226c
Nancy0221 <li.11901@osu.edu>
2022-03-10 17:26:17 -0500

    two more maps

--- sprint3/Commands/ChangeMapCommand.cs
+++ sprint3/Commands/ChangeMapCommand.cs
@@ -12,7 +12,7 @@
         {
             if (!game.changedMap)
             {
-                game.map_index = ++game.map_index % 2;
+                game.map_index = ++game.map_index % 4;
                 game.Reset();
                 game.changedMap = !game.changedMap;
             }
