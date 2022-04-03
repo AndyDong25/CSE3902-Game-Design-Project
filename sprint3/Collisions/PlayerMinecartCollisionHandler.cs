d9bbc8f9a0fdf162b3a75a36719340d42c12b0cc
cskyl <chen.9471@osu.edu>
2022-04-03 02:25:57 -0400

    add minecart class

---
+++ sprint3/Collisions/PlayerMinecartCollisionHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSE3902_CSE3902_Project.Objects.Player;
+
+namespace CSE3902_Project.Collisions
+{
+    class PlayerMinecartCollisionHandler : ICollisionHandler
+    {
+        public void HandleCollision(object o)
+        {
+            Player p = (Player)o;
+            p.xPos = p.previousXPos;
+            p.yPos = p.previousYPos;
+            p.UpdateCollider();
+        }
+    }
+}
