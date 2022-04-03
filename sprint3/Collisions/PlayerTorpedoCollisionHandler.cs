a205f126064ca9b2bb21224ff6305cbc3ae702ba
Nancy0221 <li.11901@osu.edu>
2022-04-02 23:41:51 -0400

    Add torpedo collision handler classes

---
+++ sprint3/Collisions/PlayerTorpedoCollisionHandler.cs
@@ -0,0 +1,15 @@
+﻿using CSE3902_Project.Map;
+using CSE3902_Project.Objects.Torpedo;
+
+namespace CSE3902_Project.Collisions
+{
+    class PlayerTorpedoCollisionHandler : ICollisionHandler
+    {
+        public void HandleCollision(object o)
+        {
+            Torpedo t = (Torpedo)o;
+            t.exist = false;
+            t.UpdateCollider();
+        }
+    }
+}
---
+++ sprint3/Collisions/PlayerTorpedoExplosionCollisionHandler.cs
@@ -0,0 +1,17 @@
+﻿using CSE3902_CSE3902_Project;
+using CSE3902_CSE3902_Project.Objects.Player;
+using CSE3902_Project.Objects.Torpedo;
+
+namespace CSE3902_Project.Collisions
+{
+    class PlayerTorpedoExplosionCollisionHandler : ICollisionHandler
+    {
+        public void HandleCollision(object o)
+        {
+            Player currentPlayer = (Player)o;
+            currentPlayer.currState = new PlayerDeathState(currentPlayer);
+            currentPlayer.isDead = true;
+            currentPlayer.UpdateCollider();
+        }
+    }
+}
