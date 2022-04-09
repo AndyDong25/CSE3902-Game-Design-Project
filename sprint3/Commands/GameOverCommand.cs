54dd4e8a3c61be5b7c18fe2fea44fbe668014243
Devan Mallory <32146940+dman82499@users.noreply.github.com>
2022-04-09 19:00:51 -0400

    added gameover file

---
+++ sprint3/Commands/GameOverCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace CSE3902_CSE3902_Project.Commands
+{
+    class GameOverCommand : ICommand
+    {
+        private Game1 game;
+        public GameOverCommand(Game1 game)
+        {
+            this.game = game;
+        }
+
+        public void Execute()
+        {
+            
+        }
+    }
+}
